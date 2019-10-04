2494e36368cc7dd3fe2d70676cc08f444c798446
SergioBarbosaX <serbarbos@hotmail.com>
2019-10-04 21:32:35 +0200

    Updates

---
+++ precache-manifest.c664e9d45ac3792b81e53cbecd13d376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277d250cf4803be3735ea487a6ed147e",
+    "url": "/rqm-react/index.html"
+  },
+  {
+    "revision": "99288c43cf49d8e45b30",
+    "url": "/rqm-react/static/css/main.c5a9914e.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/rqm-react/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "99288c43cf49d8e45b30",
+    "url": "/rqm-react/static/js/main.dc850d1c.chunk.js"
+  },
+  {
+    "revision": "b3062d9ca209ac8a6c10",
+    "url": "/rqm-react/static/js/runtime~main.ad2bafa8.js"
+  }
+]);
